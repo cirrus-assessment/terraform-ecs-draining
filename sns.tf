7bf4200ecef4f2ddb7ffe7401c92275243c1601e
rovnik <alexey.s@cirrusassessment.com>
2023-08-15 16:01:46 +0200

    SYSOPS-735 format names and rescrict max length

--- sns.tf
+++ sns.tf
@@ -1,5 +1,5 @@
 #tfsec:ignore:AWS016
 resource "aws_sns_topic" "topic" {
-  name = format("%s-topic", var.autoscaling_group_name)
+  name = format("%s-topic", element((regex("((([[:alnum:]]+-){3})[[:alnum:]]+)",var.autoscaling_group_name)), 0))
   tags = var.tags
 }
